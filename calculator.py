5ab49804cb6244df545e498d508c8aec0f369877
gaskahil <gaskahilde@gmail.com>
2025-03-19 15:25:06 +0100

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,14 @@
+num1 = int(input("Write a number: "))
+num2 = int(input("Write another number: "))
+
+def add():
+    nr1 = num1 + num2
+
+def subtract():
+    nr2 = num1 - num2
+
+def multiply():
+    nr3 = num1 * num2
+
+def divide():
+    nr4 = num1 // num2
